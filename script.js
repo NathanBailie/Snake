ece3d623d80c6ba1816073334f9101b3f7768c26
Nathan <nathanbailie.dev@gmail.com>
2023-03-22 11:52:08 +0300

    created basic objects of snake and apple

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 "use strict"
 
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+let cellSize = 17;
+let count = 0;
+let snake = {
+	dir: 'right',
+	body: [
+		{ x: 119, y: 100 },
+		{ x: 119 - cellSize, y: 100 },
+		{ x: 119 - cellSize * 2, y: 100 },
+	],
+};
+let apple = {
+	x: getRandomNum(0, 30) * cellSize,
+	y: getRandomNum(0, 30) * cellSize,
+};
+
+function getRandomNum(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
