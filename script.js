4943f93fc3fbf786a9534722b135bec8491f0b61
Nathan <nathanbailie.dev@gmail.com>
2023-03-22 11:55:33 +0300

    snake motion control created

--- script.js
+++ script.js
@@ -18,6 +18,34 @@ let apple = {
 	y: getRandomNum(0, 30) * cellSize,
 };
 
+document.addEventListener('keydown', (e) => {
+	if ((e.key === 'ArrowRight' ||
+		e.key === 'd' ||
+		e.key === 'в') &&
+		snake.dir !== 'left') {
+		snake.dir = 'right';
+	};
+	if ((e.key === 'ArrowLeft' ||
+		e.key === 'a' ||
+		e.key === 'ф') &&
+		snake.dir !== 'right') {
+		snake.dir = 'left';
+	};
+	if ((e.key === 'ArrowUp' ||
+		e.key === 'w' ||
+		e.key === 'ц') &&
+		snake.dir !== 'down') {
+		snake.dir = 'up';
+	};
+	if ((e.key === 'ArrowDown' ||
+		e.key === 's' ||
+		e.key === 'ы') &&
+		snake.dir !== 'up') {
+		snake.dir = 'down';
+	};
+});
+
+
 function getRandomNum(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
