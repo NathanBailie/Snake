a64a6ca1f4ee573e7cd1fbb54d8e22c210767f3c
Nathan <nathanbailie.dev@gmail.com>
2023-03-23 21:54:32 +0300

    fixed the problem with acceleration of the snake when one of the movement button is holding

--- src/modules/script.js
+++ src/modules/script.js
@@ -130,28 +130,32 @@ function toChangeTheDir(snake) {
 		if ((e.key === 'ArrowRight' ||
 			e.key === 'd' ||
 			e.key === 'в') &&
-			snake.dir !== 'left') {
+			snake.dir !== 'left' &&
+			snake.dir !== 'right') {
 			snake.dir = 'right';
 			toDrawTheMovement(snake);
 		};
 		if ((e.key === 'ArrowLeft' ||
 			e.key === 'a' ||
 			e.key === 'ф') &&
-			snake.dir !== 'right') {
+			snake.dir !== 'right' &&
+			snake.dir !== 'left') {
 			snake.dir = 'left';
 			toDrawTheMovement(snake);
 		};
 		if ((e.key === 'ArrowUp' ||
 			e.key === 'w' ||
 			e.key === 'ц') &&
-			snake.dir !== 'down') {
+			snake.dir !== 'down' &&
+			snake.dir !== 'up') {
 			snake.dir = 'up';
 			toDrawTheMovement(snake);
 		};
 		if ((e.key === 'ArrowDown' ||
 			e.key === 's' ||
 			e.key === 'ы') &&
-			snake.dir !== 'up') {
+			snake.dir !== 'up' &&
+			snake.dir !== 'down') {
 			snake.dir = 'down';
 			toDrawTheMovement(snake);
 		};
